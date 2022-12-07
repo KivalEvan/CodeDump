f1ea12b74ef275cbcbd841f10916c5597f178f4f
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2022-12-08 07:50:55 +0800

    day 777777777777777

--- advent-of-code/2022/day07/run.ts
+++ advent-of-code/2022/day07/run.ts
@@ -1,4 +1,4 @@
-export const tests = { part1: null, part2: null };
+export const tests = { part1: 95437, part2: 24933642 };
 export const settings = {
     path: '',
     test: false,
@@ -9,17 +9,59 @@ export function getInput(alternate = false) {
         settings.path +
             (settings.test ? (alternate ? 'test2.txt' : 'test.txt') : 'input.txt'),
     ).replace('\r', '');
-    return input;
+    return input.split('\n').map((s) => s.split(' '));
+}
+
+function getDirSize(input: string[][]) {
+    const directories: { [key: string]: number } = { '/': 0 };
+    let dir = '/';
+    for (const s of input) {
+        if (s[0] === '$') {
+            if (s[1] === 'cd') {
+                if (s[2] === '/') {
+                    dir = '/';
+                } else if (s[2] === '..') {
+                    dir = dir.replace(/\w*\/$/, '');
+                } else {
+                    dir += s[2] + '/';
+                }
+            }
+            continue;
+        }
+        if (s[0] === 'dir') {
+            if (!(typeof directories[dir + s[1] + '/'] === 'number')) {
+                directories[dir + s[1] + '/'] = 0;
+            }
+        }
+        if (s[0] !== 'dir') {
+            let temp = dir;
+            while (temp) {
+                directories[temp] += parseInt(s[0]);
+                temp = temp.replace(/\w*\/$/, '');
+            }
+        }
+    }
+    return directories;
 }
 
 export function part1() {
     const input = getInput();
-    return;
+    const directories = getDirSize(input);
+    return Object.values(directories)
+        .filter((v) => v <= 100_000)
+        .reduce((t, c) => t + c, 0);
 }
 
 export function part2() {
     const input = getInput();
-    return;
+    const directories = getDirSize(input);
+    const MINIMUM_SIZE = 40_000_000;
+    const CURRENT_SIZE = directories['/'];
+    const REMOVE_SIZE = CURRENT_SIZE - MINIMUM_SIZE;
+    return Object.values(directories)
+        .filter((d) => d >= REMOVE_SIZE)
+        .sort((a, b) => a - b)
+        .at(0)!;
 }
 
 if (import.meta.main) {
