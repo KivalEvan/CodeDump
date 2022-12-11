f944e904b1bbd69ef9fffa82c114d74c17078ae4
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2022-12-11 20:23:28 +0800

    day 111111111111111111

--- advent-of-code/2022/day11/run.ts
+++ advent-of-code/2022/day11/run.ts
@@ -1,4 +1,4 @@
-export const tests = { part1: null, part2: null };
+export const tests = { part1: 10605, part2: 2713310158 };
 export const settings = {
     path: '',
     test: false,
@@ -9,17 +9,85 @@ export function getInput(alternate = false) {
         settings.path +
             (settings.test ? (alternate ? 'test2.txt' : 'test.txt') : 'input.txt'),
     ).replace('\r', '');
-    return input;
+    return input.split('\n\n').map((str, _) => {
+        const parsed = str.split('\n').map((s) => s.trim());
+        const op = parsed[2].split(':')[1].trim().split(' ')[3];
+        const itself = parseInt(parsed[2].split(':')[1].trim().split(' ')[4]);
+        const obj = {
+            items: parsed[1]
+                .split(':')[1]
+                .trim()
+                .split(', ')
+                .map((n) => parseInt(n)),
+            divideBy: parseInt(parsed[3].split(' ').at(-1)!),
+            modulo: 1,
+            trueMonkee: parseInt(parsed[4].at(-1)!),
+            falseMonkee: parseInt(parsed[5].at(-1)!),
+            operation: function (x: number, divide: boolean) {
+                const result = op === '*'
+                    ? x * (!isNaN(itself) ? itself : x)
+                    : x + (!isNaN(itself) ? itself : x);
+                return (divide ? Math.floor(result / 3) : result) % this.modulo;
+            },
+            test: function (x: number) {
+                return !(x % this.divideBy) ? this.trueMonkee : this.falseMonkee;
+            },
+            inspected: 0,
+        };
+        return obj;
+    });
 }
 
 export function part1() {
-    const input = getInput();
-    return;
+    const monkee = getInput();
+    const modulo = Object.values(monkee).reduce((p, c) => p * c.divideBy, 1);
+    for (const m of monkee) {
+        m.modulo = modulo;
+    }
+    for (let round = 0; round < 20; round++) {
+        for (const id in monkee) {
+            let item = monkee[id].items.shift();
+            while (item) {
+                monkee[id].inspected++;
+                item = monkee[id].operation(item, true);
+                monkee[monkee[id].test(item)].items.push(item);
+                item = monkee[id].items.shift();
+            }
+        }
+    }
+
+    return Object.values(monkee)
+        .sort((a, b) => b.inspected - a.inspected)
+        .slice(0, 2)
+        .reduce((p, c) => p * c.inspected, 1);
 }
 
 export function part2() {
-    const input = getInput();
-    return;
+    const monkee = getInput();
+    const modulo = Object.values(monkee).reduce((p, c) => p * c.divideBy, 1);
+    for (const m of monkee) {
+        m.modulo = modulo;
+    }
+    for (let round = 0; round < 10000; round++) {
+        for (const id in monkee) {
+            let item = monkee[id].items.shift();
+            while (item) {
+                item = monkee[id].operation(item, false);
+                monkee[monkee[id].test(item)].items.push(item);
+                monkee[id].inspected++;
+
+                item = monkee[id].items.shift();
+            }
+        }
+    }
+
+    // console.log(monkee.map((m) => m.inspected));
+
+    return Object.values(monkee)
+        .sort((a, b) => b.inspected - a.inspected)
+        .slice(0, 2)
+        .map((a) => a.inspected)
+        .reduce((p, c) => p * c, 1);
 }
 
 if (import.meta.main) {
