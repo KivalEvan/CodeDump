299d2c9ede1fe622f1f3b9da7317e45563853f96
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2022-12-13 12:37:01 +0800

    day 12122112

--- advent-of-code/2022/day12/run.ts
+++ advent-of-code/2022/day12/run.ts
@@ -1,4 +1,4 @@
-export const tests = { part1: null, part2: null };
+export const tests = { part1: 31, part2: 29 };
 export const settings = {
     path: '',
     test: false,
@@ -9,17 +9,137 @@ export function getInput(alternate = false) {
         settings.path +
             (settings.test ? (alternate ? 'test2.txt' : 'test.txt') : 'input.txt'),
     ).replace('\r', '');
-    return input;
+    return input.split('\n').map((str) =>
+        str.split('').map((c) => {
+            if (c === 'S') {
+                return 0;
+            }
+            if (c === 'E') {
+                return 27;
+            }
+            return c.charCodeAt(0) - 96;
+        })
+    );
+}
+
+type CoordinateWeight = { x: number; y: number; distance: number };
+type FunctionStuff = (
+    grid: number[][],
+    position: CoordinateWeight,
+    queue: CoordinateWeight[],
+    height: number,
+) => void;
+
+const itRow = [-1, 0, 1, 0];
+const itCol = [0, -1, 0, 1];
+
+function goingUp(
+    grid: number[][],
+    position: CoordinateWeight,
+    queue: CoordinateWeight[],
+    height: number,
+) {
+    for (let i = 0; i < 4; i++) {
+        const nextX = position.x + itCol[i];
+        const nextY = position.y + itRow[i];
+        if (nextX < 0 || nextY < 0 || nextX >= grid[0].length || nextY >= grid.length) {
+            continue;
+        }
+        if (grid[nextY][nextX] - height <= 1) {
+            queue.push({
+                x: nextX,
+                y: nextY,
+                distance: position.distance + 1,
+            });
+        }
+    }
+}
+
+function goingUpReverse(
+    grid: number[][],
+    position: CoordinateWeight,
+    queue: CoordinateWeight[],
+    height: number,
+) {
+    for (let i = 0; i < 4; i++) {
+        const nextX = position.x + itCol[i];
+        const nextY = position.y + itRow[i];
+        if (nextX < 0 || nextY < 0 || nextX >= grid[0].length || nextY >= grid.length) {
+            continue;
+        }
+        if (height - grid[nextY][nextX] <= 1) {
+            queue.push({
+                x: nextX,
+                y: nextY,
+                distance: position.distance + 1,
+            });
+        }
+    }
+}
+
+function BFS(
+    fn: FunctionStuff,
+    grid: number[][],
+    start: CoordinateWeight,
+    end?: CoordinateWeight,
+) {
+    const queue: CoordinateWeight[] = [start];
+    const visited: Set<string> = new Set();
+
+    while (queue.length > 0) {
+        const position = queue.shift()!;
+        const key = `${position.x},${position.y}`;
+
+        if (visited.has(key)) {
+            continue;
+        }
+
+        visited.add(key);
+
+        if (end) {
+            if (position.x === end.x && position.y === end.y) {
+                return position.distance;
+            }
+        } else if (grid[position.y][position.x] === 1) {
+            return position.distance;
+        }
+
+        const height = grid[position.y][position.x];
+
+        fn(grid, position, queue, height);
+    }
 }
 
 export function part1() {
-    const input = getInput();
-    return;
+    const grid = getInput();
+
+    const startRow = grid.findIndex((row) => row.some((column) => column === 0));
+    const startCoordinate: CoordinateWeight = {
+        x: grid[startRow].findIndex((column) => column === 0),
+        y: startRow,
+        distance: 0,
+    };
+    const endRow = grid.findIndex((row) => row.some((column) => column === 27));
+    const endCoordinate: CoordinateWeight = {
+        x: grid[endRow].findIndex((column) => column === 27),
+        y: endRow,
+        distance: 0,
+    };
+
+    return BFS(goingUp, grid, startCoordinate, endCoordinate);
 }
 
 export function part2() {
-    const input = getInput();
-    return;
+    const grid = getInput();
+
+    const startRow = grid.findIndex((row) => row.some((column) => column === 27));
+    const startCoordinate: CoordinateWeight = {
+        x: grid[startRow].findIndex((column) => column === 27),
+        y: startRow,
+        distance: 0,
+    };
+
+    return BFS(goingUpReverse, grid, startCoordinate);
 }
 
 if (import.meta.main) {
